6a3303ee093075f77fa3da2a6fea7d647f250778
kyunghee kwak <kwakkyunghee325@gmail.com>
2023-05-25 09:57:43 +0900

    정석적인 피보나치 수열

---
+++ RecursionPrac/Fibonacci.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace RecursionPrac
+{
+    internal class Fibonacci
+    {
+        public long Get(int n)
+        {//Get(3) -> Get(2)+Get(1)
+            Console.WriteLine(n);
+            if(n < 0) { return 0; }
+            if(n == 1) {  return 1; }
+            return Get(n-2)+Get(n-1);
+        }
+    }
+}
